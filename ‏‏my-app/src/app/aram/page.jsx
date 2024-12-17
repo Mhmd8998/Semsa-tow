f5b3f43759b6d82cf90fa27ed3b5e01607533182
Mhmd8998 <alnasrmhmd9988@gmail.com>
2024-12-17 17:10:20 +0300

    Update page.jsx

--- ‏‏my-app/src/app/aram/page.jsx
+++ ‏‏my-app/src/app/aram/page.jsx
@@ -33,7 +33,7 @@ const Page = () => {
   const PostToDiscord = () => {
     const description = Object.entries(form.data)
       .map((d) => `${d[0]} : ${d[1]} `)
-      .join("%0A");
+      .join("\n");
     Send(description)
     
   };
